c8517092e338537a84d208d05e50ad3dde2cd0ff
www-data <www-data@vps-1081345.vpshome.pro>
2017-03-06 11:31:27 +0300

    dbchange

--- forging/wsgi.py
+++ forging/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forging.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forging.settings_local")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
